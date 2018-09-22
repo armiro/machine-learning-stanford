a19e11c3bec3452fa6e0039a8c43dc5f086f6365
armiro <arman.hgh.1374@gmail.com>
2018-09-23 01:19:16 +0330

    delete redundant comments

--- Linear Regression/featureNormalize.m
+++ Linear Regression/featureNormalize.m
@@ -1,46 +1,23 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
-%FEATURENORMALIZE Normalizes the features in X 
-%   FEATURENORMALIZE(X) returns a normalized version of X where
-%   the mean value of each feature is 0 and the standard deviation
-%   is 1. This is often a good preprocessing step to do when
-%   working with learning algorithms.
 
-% You need to set these values correctly
+% Initialize some useful values
 n = size(X, 2);
 m = size(X, 1);
 X_norm = X;
 mu = zeros(1, n);
 sigma = zeros(1, n);
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: First, for each feature dimension, compute the mean
-%               of the feature and subtract it from the dataset,
-%               storing the mean value in mu. Next, compute the 
-%               standard deviation of each feature and divide
-%               each feature by it's standard deviation, storing
-%               the standard deviation in sigma. 
-%
-%               Note that X is a matrix where each column is a 
-%               feature and each row is an example. You need 
-%               to perform the normalization separately for 
-%               each feature. 
-%
-% Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
 for feature = 1:n
   
+  % computing mu and sigma for each feature (each column of X variable)
   mu(feature) = mean(X(:, feature));
   sigma(feature) = std(X(:, feature));
   
+  % substituting each sample's value with its normalized value
   for sample = 1:m
     X_norm(sample, feature) = (X(sample, feature) - mu(feature)) / sigma(feature);
   end
   
 end
-
-
-
-% ============================================================
 
 end
