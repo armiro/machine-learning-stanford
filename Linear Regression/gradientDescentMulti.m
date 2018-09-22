6a78a346866c8ff3f56184c19ce54112c14dbac4
armiro <arman.hgh.1374@gmail.com>
2018-09-23 01:20:10 +0330

    under development

--- Linear Regression/gradientDescentMulti.m
+++ Linear Regression/gradientDescentMulti.m
@@ -1,37 +1,18 @@
 function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENTMULTI Performs gradient descent to learn theta
-%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) updates theta by
-%   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
 m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
 
-for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
-
-
-
-
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
-
+% first we need to normalize 'y' values
+mu_y = mean(y(:));
+sigma_y = std(y(:));
+for sample = 1:m
+  y(sample) = (y(sample) - mu_y) / sigma_y;
 end
+
+% here we can benefit from gradientDescent function, which is independent
+% from the number of features
+[theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters);
+
 
 end
