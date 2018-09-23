b35a20fcfa22435bf017eb55b0d7d3214b582da9
armiro <arman.hgh.1374@gmail.com>
2018-09-23 16:24:06 +0330

    remove redundant comments

--- Linear Regression/featureNormalize.m
+++ Linear Regression/featureNormalize.m
@@ -2,22 +2,13 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 
 % Initialize some useful values
 n = size(X, 2);
-m = size(X, 1);
+%m = size(X, 1);
 X_norm = X;
 mu = zeros(1, n);
 sigma = zeros(1, n);
 
-for feature = 1:n
-  
-  % computing mu and sigma for each feature (each column of X variable)
-  mu(feature) = mean(X(:, feature));
-  sigma(feature) = std(X(:, feature));
-  
-  % substituting each sample's value with its normalized value
-  for sample = 1:m
-    X_norm(sample, feature) = (X(sample, feature) - mu(feature)) / sigma(feature);
-  end
-  
-end
+mu = mean(X);
+sigma = std(X);
+X_norm = (X - mu) ./ std(X);
 
 end
